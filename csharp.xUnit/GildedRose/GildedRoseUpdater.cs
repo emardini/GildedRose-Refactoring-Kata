c192f522a0730093a17b279e17625cecbdc06d3b
emardini <emardini@costar.com>
2025-05-16 08:38:01 -0400

    Refactor code

---
+++ csharp.xUnit/GildedRose/GildedRoseUpdater.cs
@@ -0,0 +1,100 @@
+﻿namespace GildedRoseKata;
+
+public static class GildedRoseUpdater
+{
+    public static void UpdateQualityItem(this Item item)
+    {
+        if (item.Name == "Aged Brie")
+        {
+            UpdateAgedBrie(item);
+            return;
+        }
+
+        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+        {
+            UpdateBackstagePasses(item);
+            return;
+        }
+
+        if (item.Name == "Sulfuras, Hand of Ragnaros")
+        {
+            return;
+        }
+
+        UpdateDefault(item);
+    }
+
+    private static void UpdateDefault(Item item)
+    {
+        if (item.Quality > 0)
+        {
+            item.Quality--;
+        }
+
+        item.SellIn--;
+
+
+        if (item.SellIn < 0)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality--;
+            }
+        }
+    }
+
+    private static void UpdateBackstagePasses(Item item)
+    {
+        if (item.SellIn <= 0)
+        {
+            item.SellIn--;
+            item.Quality = 0;
+            return;
+        }
+
+        if (item.Quality < 50)
+        {
+            item.Quality++;
+
+            if (item.SellIn < 11)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+            }
+
+            if (item.SellIn < 6)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+            }
+        }
+
+        item.SellIn--;
+
+        return;
+    }
+
+    private static void UpdateAgedBrie(Item item)
+    {
+        if (item.Quality < 50)
+        {
+            item.Quality++;
+        }
+
+        item.SellIn--;
+
+        if (item.SellIn < 0)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+            }
+        }
+
+        return;
+    }
+}
--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -1,17 +1,52 @@
-﻿using Xunit;
-using System.Collections.Generic;
+﻿using FluentAssertions;
 using GildedRoseKata;
+using Xunit;
 
 namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
     [Fact]
-    public void foo()
+    public void WhenAnItemIsUpdatedThenTheNameDoesNotChange()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
-        app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        var item = new Item  { Name = "foo", SellIn = 0, Quality = 0 };
+        item.UpdateQualityItem();
+        item.Name.Should().Be("foo");
+    }
+
+    [Theory]
+    [InlineData("Aged Brie", 1, 49, 0, 50)]  
+    [InlineData("Aged Brie", 0, 49, -1, 50)]
+    [InlineData("Aged Brie", 0, 48, -1, 50)]
+    [InlineData("Aged Brie", 0, 1, -1, 3)]
+    [InlineData("Aged Brie", 0, 50, -1, 50)]
+    [InlineData("Aged Brie", 1, 50, 0, 50)]
+    [InlineData("Aged Brie", 1, 0, 0, 1)]
+
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 0, 47, -1, 0)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 47, 0, 50)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 10, 47, 9, 49)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 47, 10, 48)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 50, 10, 50)]
+    [InlineData("Backstage passes to a TAFKAL80ETC concert", -1, 47, -2, 0)]
+
+    [InlineData("Sulfuras, Hand of Ragnaros", -1, 47, -1, 47)]
+    [InlineData("Sulfuras, Hand of Ragnaros", -1, -1, -1, -1)]
+    [InlineData("Sulfuras, Hand of Ragnaros", 2, 4, 2, 4)]
+
+    [InlineData("Default", 1, 1, 0, 0)]
+    [InlineData("Default", 2, 2, 1, 1)]
+    [InlineData("Default", 0, 4, -1, 2)]
+    [InlineData("Default", 10, 0, 9, 0)]
+
+    public void SellinAndQualityAreProperlyUpdated(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    {
+        var item = new Item {Name = name, SellIn = sellIn, Quality = quality } ;
+
+        item.UpdateQualityItem();
+
+        var expectedItem = new Item { Name = name, SellIn=expectedSellIn, Quality = expectedQuality };
+
+        item.Should().BeEquivalentTo(expectedItem);
     }
 }
