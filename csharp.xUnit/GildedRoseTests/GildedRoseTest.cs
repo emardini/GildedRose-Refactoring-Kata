3fd72d8d630e94017144af67df225c24d0300d1a
emardini <emardini@costar.com>
2025-07-09 17:04:04 -0400

    Changes from Engineering Dojo

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -1,52 +1,83 @@
-﻿using FluentAssertions;
+﻿using Xunit;
+using System.Collections.Generic;
 using GildedRoseKata;
-using Xunit;
+using System.ComponentModel;
 
 namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
     [Fact]
-    public void WhenAnItemIsUpdatedThenTheNameDoesNotChange()
+    public void Foo()
     {
-        var item = new Item  { Name = "foo", SellIn = 0, Quality = 0 };
-        item.UpdateQualityItem();
-        item.Name.Should().Be("foo");
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("foo", Items[0].Name);
     }
 
     [Theory]
-    [InlineData("Aged Brie", 1, 49, 0, 50)]  
-    [InlineData("Aged Brie", 0, 49, -1, 50)]
-    [InlineData("Aged Brie", 0, 48, -1, 50)]
-    [InlineData("Aged Brie", 0, 1, -1, 3)]
-    [InlineData("Aged Brie", 0, 50, -1, 50)]
-    [InlineData("Aged Brie", 1, 50, 0, 50)]
-    [InlineData("Aged Brie", 1, 0, 0, 1)]
-
-    [InlineData("Backstage passes to a TAFKAL80ETC concert", 0, 47, -1, 0)]
-    [InlineData("Backstage passes to a TAFKAL80ETC concert", 1, 47, 0, 50)]
-    [InlineData("Backstage passes to a TAFKAL80ETC concert", 10, 47, 9, 49)]
-    [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 47, 10, 48)]
-    [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 50, 10, 50)]
-    [InlineData("Backstage passes to a TAFKAL80ETC concert", -1, 47, -2, 0)]
-
-    [InlineData("Sulfuras, Hand of Ragnaros", -1, 47, -1, 47)]
-    [InlineData("Sulfuras, Hand of Ragnaros", -1, -1, -1, -1)]
-    [InlineData("Sulfuras, Hand of Ragnaros", 2, 4, 2, 4)]
-
-    [InlineData("Default", 1, 1, 0, 0)]
-    [InlineData("Default", 2, 2, 1, 1)]
-    [InlineData("Default", 0, 4, -1, 2)]
-    [InlineData("Default", 10, 0, 9, 0)]
-
-    public void SellinAndQualityAreProperlyUpdated(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(-1, -1)]
+    public void WhenItemIsConjuredQualityLessThanTwoThenQualityWontDecrease(int initialQuality, int expectedQuality)
     {
-        var item = new Item {Name = name, SellIn = sellIn, Quality = quality } ;
+        IList<Item> Items = new List<Item> { new Item { Name = "Conjured Item 001", SellIn = 0, Quality = initialQuality } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(expectedQuality, Items[0].Quality);
+    }
 
-        item.UpdateQualityItem();
+    [Theory]
+    [InlineData(2, 0)]
+    [InlineData(10, 8)]
+    public void WhenItemIsConjuredQualityMoreOrEqualThanTwoThenQualityDecreaseByTwo(int initialQuality, int expectedQuality)
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Conjured Item 001", SellIn = 0, Quality = initialQuality } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(expectedQuality, Items[0].Quality);
+    }
 
-        var expectedItem = new Item { Name = name, SellIn=expectedSellIn, Quality = expectedQuality };
 
-        item.Should().BeEquivalentTo(expectedItem);
+    [Theory]
+    [InlineData(-1, -1)]
+    [InlineData(0, 0)]
+    [InlineData(10, 10)]
+    [InlineData(80, 80)]
+    public void WhenItemIsSulfurasQualityDoesNotChange(int initialQuality, int expectedQuality)
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = initialQuality } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(expectedQuality, Items[0].Quality);
+    }
+
+    [Theory]
+    [InlineData(-1, -1)]
+    [InlineData(0, 0)]
+    [InlineData(10, 10)]
+    [InlineData(80, 80)]
+    public void WhenItemIsSulfurasSellingDoesNotChange(int initialSellin, int expectedSellIn)
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = initialSellin, Quality = 10 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(expectedSellIn, Items[0].SellIn);
+    }
+
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(-1, 0)]
+    [InlineData(-5, -4)]
+    [InlineData(49, 50)]
+    [InlineData(50, 50)]
+    public void WhenItemIsAgedBrieQualityLessThanFiftyAndSellinMoreThanZeroIncreaseByOne(int initialQuality, int expectedQuality )
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = initialQuality } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(expectedQuality, Items[0].Quality);
     }
 }
